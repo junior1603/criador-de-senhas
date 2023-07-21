3263bf03c01e6d3766d1fd783159f950729b03b4
JONASANTOS <jonasantos16037@gmail.com>
2023-07-21 16:39:44 -0300

    criador de senhas

---
+++ script.js
@@ -0,0 +1,32 @@
+let sliderElement = document.querySelector("#slider")
+let buttonElement = document.querySelector("#button")
+
+let sizePassword = document.querySelector("#valor")
+let password = document.querySelector("#password")
+
+let comtainerPassword = document.querySelector("#comtainer-password")
+
+let charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%¨&*"
+let novaSenha = ""
+
+sizePassword.innerHTML = sliderElement.value 
+
+slider.oninput = function(){
+    sizePassword.innerHTML = this.value
+}
+
+function generatePassword(){
+    let pass = ""
+    for(let i = 0, n = charset.length; i < sliderElement.value; ++i){
+        pass += charset.charAt(Math.floor(Math.random() * n))
+    }
+    comtainerPassword.classList.remove("hide")
+    password.innerHTML = pass
+    novaSenha = pass
+
+}
+
+function copyPassword(){
+    alert("Senha copiada!")
+    navigator.clipboard.writeText(novaSenha)
+}
